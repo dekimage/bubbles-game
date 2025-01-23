39f655be3da0fa200862dd8743a1ea4ece1ba54d
dekimage <dejan.gavrilovikk@gmail.com>
2025-01-23 21:39:32 +0100

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,19 +3,12 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "../globals.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import ReusableLayout from "@/reusable-ui/ReusableLayout";
-import { useEffect } from "react";
-import MobxStore from "@/mobx";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    // Set up habit resets
-    MobxStore.setupHabitResets();
-    // Initial fetch of habits
-    MobxStore.fetchHabits();
-  }, []);
 
   return (
     <html lang="en">
